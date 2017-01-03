37b312476ea7944b0b7204fd9242a1fd637f9cf8
Andrew Redd <andrew.redd@hsc.utah.edu>
2017-01-03 16:59:07 -0700

    added Documentation-Keyword class to enforce valid keywords.

---
+++ R/Class-Documentation-Keyword.R
@@ -0,0 +1,38 @@
+
+
+keyword.db <- local({
+keyword.lines <- readLines(normalizePath(file.path(R.home(), 'doc', 'KEYWORDS.db')))
+keyword.descriptions <- gsub("^\\s+", "", sapply(strsplit(keyword.lines, ':', fixed=TRUE), getElement, 2))
+
+keyword.heirarchy   <- strsplit(sapply(strsplit(keyword.lines, ':', fixed=TRUE), head, 1), '|', fixed=TRUE)
+keyword             <- sapply( keyword.heirarchy, tail, 1)
+keyword.parent.list <- sapply(sapply( keyword.heirarchy, tail, -1), tail, 1)
+keyword.parent      <- unlist(ifelse( sapply( keyword.parent.list, length), keyword.parent.list, NA_character_))
+    
+db <- 
+data.frame( KEYWORD     = keyword
+          , PARENT      = keyword.parent
+          , DESCRIPTION = keyword.descriptions
+          )
+rownames(db) = keyword
+db    
+})
+
+setClass("Documentation-Keyword", contains='character'
+        , validity = function(object){
+                is.valid <- object %in% keyword.db$KEYWORD
+                if(all(is.valid)) return(TRUE)
+                paste("the following are not valid keywords:", comma_list(object[!is.valid]))
+            }
+        )
+if(FALSE){#! @testing
+    x <- new('Documentation-Keyword', 'utilities')
+    expect_true(validObject(x))
+    
+    expect_error(new('Documentation-Keyword', 'utils'))
+    
+    validObject(as('utilities', 'Documentation-Keyword'))
+    
+    
+    
+}
