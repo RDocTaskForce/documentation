f5df44de87440c59708a0d480c8d641fc8ae1033
Andrew Redd <andrew.redd@hsc.utah.edu>
2018-07-31 12:51:06 -0600

    Expanded capability of infix concatenate operators.

--- R/util-infix.R
+++ R/util-infix.R
@@ -14,26 +14,39 @@
 #' }
 #' @aliases %\\%
 #' @export %\% %<<% %<<<%
-`%<<%` <- function(lhs, rhs)paste(lhs, rhs, sep=" ")
+`%<<%` <- function(lhs, rhs){
+    if (is.null(rhs)) return(collapse(lhs))
+    else if (is.null(lhs)) return(collapse(rhs))
+    else return(paste(collapse(lhs), collapse(rhs), sep=" "))
+}
 if(FALSE){#! @testing %<<%
     a <- 'A vain chalks above the integrated biscuit. '
     b <- 'Within the ground burns the leader.'
     c <- 'How can the fifteen distress lose?'
     expect_equal(a %<<% b, paste(a,b))
     expect_equal(a %<<% b %<<% c, paste(a,b,c))
+    
+    expect_equal(a %<<% NULL, a)
+    expect_equal(NULL %<<% a, a)
+    expect_equal(NULL %<<% NULL, "")
 }
 
 #' @rdname documentation-infix-operators
-`%<<<%` <- function(lhs, rhs)paste(lhs, rhs, sep="")
+`%<<<%` <- function(lhs, rhs) paste(collapse0(lhs), collapse(rhs), sep="")
+
 if(FALSE){#! @testing %<<<%
     a <- 'A vain chalks above the integrated biscuit. '
     b <- ' Within the ground burns the leader.'
     c <- 'How can the fifteen distress lose?'
-    expect_equal(a %\% b, paste(a,b, sep='\n'))
-    expect_equal(a %\% b %\% c, paste(a,b, c, sep='\n'))
+    expect_equal(a %<<<% b, paste0(a,b))
+    expect_equal(a %<<<% b %<<<% c, paste0(a,b, c, sep=''))
+    
+    expect_equal(a %<<<% NULL, a)
+    expect_equal(NULL %<<<% a, a)
+    expect_equal(NULL %<<<% NULL, '')
 }
 
-`%\\%` <- function(lhs, rhs)paste(lhs, rhs, sep="\n")
+`%\\%` <- function(lhs, rhs) paste(collapse(lhs, '\n'), collapse(rhs, '\n'), sep="\n")
 if(FALSE){#! @testing newline-concatenation
     a <- 'A vain chalks above the integrated biscuit. '
     b <- ' Within the ground burns the leader.'
@@ -42,4 +55,8 @@ if(FALSE){#! @testing newline-concatenation
     expect_equal(a %\% b %\% c, paste(a,b, c, sep='\n'))
 }
 
-
+`%||%` <- function (x, y) if (is.null(x)) y else x
+if(FALSE){#@testing
+    expect_true( NULL %||% TRUE)
+    expect_true( TRUE %||% FALSE)
+}
