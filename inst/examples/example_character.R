a2d24c8828c42ef154a3634eeba5f3b4aba5b75d
Andrew Redd <andrew.redd@hsc.utah.edu>
2018-08-03 15:47:38 +0000

    Added missing example files.

---
+++ inst/examples/example_character.R
@@ -0,0 +1,10 @@
+
+#' An example character vector
+#'
+#' This example is used to test extracting documentation from
+#' a character vector which typically does not contain
+#' a source reference.
+example_character <-
+    c( "first"  #< the first element
+     , "second" #< the second element
+     )
---
+++ inst/examples/example_function2.R
@@ -0,0 +1,11 @@
+#' Example function 2
+#'
+#' This is used to check for relative comment association.
+example_function2 <-
+function( x #< The x argument description.
+        , y #< The y argument description
+            #< takes 2 lines.
+        ){
+    x+y
+    #< an orphaned relative comment.
+}
