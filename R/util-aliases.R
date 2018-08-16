47550ca85f6968ebd79a9ecf55b92bc09b62d445
Andrew Redd` <amredd@gmail.com>
2018-08-16 15:16:40 -0600

    upgraded s alias.

--- R/util-aliases.R
+++ R/util-aliases.R
@@ -8,7 +8,23 @@
 }
 
 #' alias for structure
-s <- structure
+s <- function(x, ...){
+    new.attr <- list(...)
+    if (is.null(names(new.attr)))
+        names(new.attr) <- as.character(substitute(c(...)))[-1]
+    else if(any(. <- is.na(names(new.attr)) | names(new.attr) == ''))
+        names(new.attr) <- ifelse(., as.character(substitute(c(...)))[-1], names(new.attr))
+    attributes(x) <- new.attr
+    return(x)
+}
+if(FALSE){#@testing
+    msg <- "An failure message"
+    val <- s(FALSE, msg, count = 5)
+    expect_identical(attributes(val), list(msg=msg, count=5))
+    
+}
+
+
 
 #' Equivalent to add_class
 cl <- function(x, new)s(x, class=c(new, class(s)))
