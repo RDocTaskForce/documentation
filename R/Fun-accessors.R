097c59eb096616171afe58370113f3f84ead413e
Andrew Redd <andrew.redd@hsc.utah.edu>
2018-09-24 20:18:49 +0000

    testing Fun-accessors document checking is conditional now.

--- R/Fun-accessors.R
+++ R/Fun-accessors.R
@@ -56,25 +56,27 @@ for (i in seq_along(all.documentation.slots)){
 .define_generic_doc_accessor('details', 'FormattedText')
 
 if(FALSE){#@testing generic accessors
-    expect_is(doc <- documentation(doc_get_name), 'function-Documentation')
-    expect_equal(doc@title, "Documentation accessor for name")
-    expect_equal( doc@description
-                , FormattedText("This is an automatically generated function for accessing the" %<<%
-                                "name of a documentation object." %<<%
-                                "Specific methods may override the default behavior, " %<<%
-                                "especially when the name" %<<%
-                                "is expected to conform to a standard" %<<%
-                                "or is generated form other known information."))
+    if (.document.generated){
+        expect_is(doc <- documentation(doc_get_name), 'function-Documentation')
+        expect_equal(doc@title, "Documentation accessor for name")
+        expect_equal( doc@description
+                    , FormattedText("This is an automatically generated function for accessing the" %<<%
+                                    "name of a documentation object." %<<%
+                                    "Specific methods may override the default behavior, " %<<%
+                                    "especially when the name" %<<%
+                                    "is expected to conform to a standard" %<<%
+                                    "or is generated form other known information."))
 
-    expect_is(doc <- documentation(doc_get_title), 'function-Documentation')
-    expect_equal(doc@title, "Documentation accessor for title")
-    expect_equal( doc@description
-                , FormattedText("This is an automatically generated function for accessing the" %<<%
-                                "title of a documentation object." %<<%
-                                "Specific methods may override the default behavior, " %<<%
-                                "especially when the title" %<<%
-                                "is expected to conform to a standard" %<<%
-                                "or is generated form other known information."))
+        expect_is(doc <- documentation(doc_get_title), 'function-Documentation')
+        expect_equal(doc@title, "Documentation accessor for title")
+        expect_equal( doc@description
+                    , FormattedText("This is an automatically generated function for accessing the" %<<%
+                                    "title of a documentation object." %<<%
+                                    "Specific methods may override the default behavior, " %<<%
+                                    "especially when the title" %<<%
+                                    "is expected to conform to a standard" %<<%
+                                    "or is generated form other known information."))
+    }
 }
 
 
--- tests/testthat/test-Fun-accessors.R
+++ tests/testthat/test-Fun-accessors.R
@@ -4,23 +4,25 @@
 context('tests extracted from file `Fun-accessors.R`')
 #line 58 "R/Fun-accessors.R"
 test_that('generic accessors', {#@testing generic accessors
-    expect_is(doc <- documentation(doc_get_name), 'function-Documentation')
-    expect_equal(doc@title, "Documentation accessor for name")
-    expect_equal( doc@description
-                , FormattedText("This is an automatically generated function for accessing the" %<<%
-                                "name of a documentation object." %<<%
-                                "Specific methods may override the default behavior, " %<<%
-                                "especially when the name" %<<%
-                                "is expected to conform to a standard" %<<%
-                                "or is generated form other known information."))
+    if (.document.generated){
+        expect_is(doc <- documentation(doc_get_name), 'function-Documentation')
+        expect_equal(doc@title, "Documentation accessor for name")
+        expect_equal( doc@description
+                    , FormattedText("This is an automatically generated function for accessing the" %<<%
+                                    "name of a documentation object." %<<%
+                                    "Specific methods may override the default behavior, " %<<%
+                                    "especially when the name" %<<%
+                                    "is expected to conform to a standard" %<<%
+                                    "or is generated form other known information."))
 
-    expect_is(doc <- documentation(doc_get_title), 'function-Documentation')
-    expect_equal(doc@title, "Documentation accessor for title")
-    expect_equal( doc@description
-                , FormattedText("This is an automatically generated function for accessing the" %<<%
-                                "title of a documentation object." %<<%
-                                "Specific methods may override the default behavior, " %<<%
-                                "especially when the title" %<<%
-                                "is expected to conform to a standard" %<<%
-                                "or is generated form other known information."))
+        expect_is(doc <- documentation(doc_get_title), 'function-Documentation')
+        expect_equal(doc@title, "Documentation accessor for title")
+        expect_equal( doc@description
+                    , FormattedText("This is an automatically generated function for accessing the" %<<%
+                                    "title of a documentation object." %<<%
+                                    "Specific methods may override the default behavior, " %<<%
+                                    "especially when the title" %<<%
+                                    "is expected to conform to a standard" %<<%
+                                    "or is generated form other known information."))
+    }
 })
