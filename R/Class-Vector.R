55560f3f293436ab9e4edb5f9e82fb22106b91ac
Andrew Redd <andrew.redd@hsc.utah.edu>
2018-10-19 21:12:39 +0000

    Added reference vector support
    * Added refList class, the base class for refVectors.
    * added setRefVector() to create reference vectors, which contain static reference variables.

--- R/Class-Vector.R
+++ R/Class-Vector.R
@@ -1,11 +1,12 @@
 #' @include utils.R
+#' @include Class-StaticRefClasses.R
 
 #' A Vector with only one type of object
 #' @export
 setVector <-
 function( element                                   #< name of the class that elements of the vector must inherit from.
         , Class   = paste0("Vector(", element, ")") #< Name of the class
-        , ...                                       #< passed onto SetClass, excluding contains, validity
+        , ...                                       #< passed on, excluding contains, validity
         , contains = character()
         , c.element = TRUE
         , safe.replace = TRUE
@@ -87,8 +88,8 @@ if(FALSE){#! @testing
     expect_is(new.class, "classGeneratorFunction")
 
     expect_is(name.vector <- new.class(), 'Vector(name)')
-    name.vector[[1]] <- as.name('a')
-    name.vector[[2]] <- as.name('b')
+    name.vector[[1]] <- 'a'
+    name.vector[[2]] <- 'b'
     expect_equal(length(name.vector), 2)
     expect_true(validObject(name.vector))
     expect_error( withr::with_options( list(useFancyQuotes=FALSE)
@@ -100,6 +101,7 @@ if(FALSE){#! @testing
                                      )
                 , "value does not inherit from class 'Vector\\(name\\)', nor can it be coerced\\.")
 
+    expect_error(name.vector[3] <- 'c')
     name.vector[3] <- as.name('c')
 
     x <- new.class(list( a <- as.name('a')
@@ -124,6 +126,12 @@ if(FALSE){#! @testing
     expect_error( new('Vector(name)', list(as.name('a'), as.name('b'), 'c'))
                 , "Element of Vector at position 3 is not a name")
 
+
+    y <- new.class(list( as.name('a')))
+    z <- c(y, 'b')
+    expect_is(z, "Vector(name)")
+    expect_length(z, 2L)
+
     removeVector(new.class@className, where=globalenv())
 }
 if(FALSE){#@testing setVector w/ Virtual Class
@@ -144,7 +152,7 @@ if(FALSE){#@testing setVector w/ Virtual Class
     expect_is_exactly(z, vclass@className)
     expect_is(z, velement@className)
 
-    removeVector(vclass)
+    expect_true(removeVector(vclass, where = globalenv()))
 }
 
 
@@ -159,5 +167,126 @@ removeVector <- function(Class, where=topenv(parent.frame())){
                  )
       ,  envir = where)
     removeClass(Class, where=where)
+}
+
+# Reference Vectors -----
+
+### Class: refList #####
+refList <- setRefClass( 'refList', fields = list(.='list'),
+    methods = list(initialize = function(...){
+        . <<- list(...)
+    }))
+setMethod('[[', c('refList', 'ANY'), function(x, i, ...)(x$.)[[i, ...]])
+setMethod('[[<-', c('refList', 'ANY'), function(x, i, ..., value)x$.[[i, ...]] <- value)
+setMethod('length', 'refList', function(x)length(x$.))
+setMethod('names', 'refList', function(x)names(x$.))
+if(FALSE){#@testing
+    val <- refList('a', 1L, TRUE)
+    expect_is(val, 'refList')
+    expect_is(val, 'envRefClass')
+
+    expect_identical(val[[1]], 'a')
+    expect_identical(val[[2]], 1L)
+    expect_identical(val[[3]], TRUE)
+
+    y <- val
+    y[[4]] <- 'testing'
+    expect_identical(val[[4]], 'testing')
+
+    expect_length(val, 4L)
+    expect_null(names(val))
+}
+
+
+### Helper Functions #####
+.validity_via_ref_method <- function(object){object$validate()}
+
+### Reference Vectors #####
+#' @describeIn setVector Create a reference class based vector.
+setRefVector <-
+function( element
+        , Class = paste0("RefVector(", element, ")")
+        , fields = list()
+        , contains = character()
+        , methods = list()
+        , ...
+        , add.method = TRUE
+        , vector.is.element = isVirtualClass(element)
+        , restrict.unique = FALSE
+        , must.be.named = NA #< TRUE means there must be names,
+                             #< FALSE means cannot have names,
+                             #< NA means no enforcement
+        , where = topenv(parent.frame())
+        ){
+    where <- where
+    assert_that( is.string(element), is.string(Class)
+               , isNamespace(where) || identical(where, globalenv())
+               )
+    if (!('initialize' %in% names(methods)))
+        methods[["initialize"]] <- function(...){
+            l <- list(...)
+            for (i in seq_along(l)) {
+                if (!is(l[[i]], element))
+                    try(l[[i]] <- as(l[[i]], element))
+            }
+            assert_that(all_inherit(l, element, "`...`"))
+            . <<- l
+            invisible(.self)
+        }
+    if (!('validate' %in% names(methods)))
+        methods[['validate']] <- function(){
+            validate_that(all_inherit(., element))
+        }
+    if (!('is_valid' %in% names(methods)))
+        methods[['is_valid']] <- function(){
+            valid <- validate()
+            if (isTRUE(valid)) return(valid) else
+            return(s(FALSE, msg=valid))
+        }
+    if (!('add' %in% names(methods)))
+        methods[['add']] <- function(...){
+            l <- list(...)
+            for (i in rev(seq_along(l))) {
+                if (!is(l[[i]], element))
+                    l[[i]] <- as(l[[i]], element)
+            }
+            assert_that(all_inherit(l, element, "`...`"))
+            . <<- c(., l)
+            invisible(.self)
+        }
+    generator <-
+        setStaticRefClass( Class=Class
+                         , static.const = list(element=element)
+                         , fields = fields
+                         , contains = c(contains, 'refList')
+                         , methods = methods
+                         , validity = .validity_via_ref_method
+                         , ..., where=where)
+    return(invisible(generator))
+}
+if(FALSE){#@testing
+    test_vector <- setRefVector('logical')
+    expect_is(test_vector, 'refObjectGenerator')
+
+    bare <- test_vector()
+    expect_is(bare, "RefVector(logical)")
+    expect_length(bare, 0L)
+    expect_identical(get('element', bare), 'logical')
+
+    val <- test_vector(a=TRUE, b=FALSE)
+    expect_is(val, "RefVector(logical)")
+    expect_identical(get('element', val), 'logical')
+    expect_length(val, 2L)
+    expect_identical(val[[1]], TRUE)
+    expect_identical(val[['a']], TRUE)
+    expect_identical(val[[2]], FALSE)
+    expect_identical(val[['b']], FALSE)
+
+    new <- c(c=NA)
+    val$add(c=NA)
+    expect_length(val, 3L)
+    expect_identical(val[[3]], NA)
+
+    expect_true(val$is_valid())
 }
 
--- tests/testthat/test-Class-Vector.R
+++ tests/testthat/test-Class-Vector.R
@@ -2,14 +2,14 @@
 #! Changes will be overwritten.
 
 context('tests extracted from file `Class-Vector.R`')
-#line 84 "/rdtf/documentation/R/Class-Vector.R"
+#line 86 "/rdtf/documentation/R/Class-Vector.R"
 test_that('setVector', {#! @testing
     new.class <- setVector('name', where=globalenv())
     expect_is(new.class, "classGeneratorFunction")
 
     expect_is(name.vector <- new.class(), 'Vector(name)')
-    name.vector[[1]] <- as.name('a')
-    name.vector[[2]] <- as.name('b')
+    name.vector[[1]] <- 'a'
+    name.vector[[2]] <- 'b'
     expect_equal(length(name.vector), 2)
     expect_true(validObject(name.vector))
     expect_error( withr::with_options( list(useFancyQuotes=FALSE)
@@ -21,6 +21,7 @@ test_that('setVector', {#! @testing
                                      )
                 , "value does not inherit from class 'Vector\\(name\\)', nor can it be coerced\\.")
 
+    expect_error(name.vector[3] <- 'c')
     name.vector[3] <- as.name('c')
 
     x <- new.class(list( a <- as.name('a')
@@ -45,9 +46,15 @@ test_that('setVector', {#! @testing
     expect_error( new('Vector(name)', list(as.name('a'), as.name('b'), 'c'))
                 , "Element of Vector at position 3 is not a name")
 
+
+    y <- new.class(list( as.name('a')))
+    z <- c(y, 'b')
+    expect_is(z, "Vector(name)")
+    expect_length(z, 2L)
+
     removeVector(new.class@className, where=globalenv())
 })
-#line 128 "/rdtf/documentation/R/Class-Vector.R"
+#line 137 "/rdtf/documentation/R/Class-Vector.R"
 test_that('setVector w/ Virtual Class', {#@testing setVector w/ Virtual Class
     velement <- setClass('virtual element', where = globalenv())
     expect_true(isVirtualClass(velement@className))
@@ -66,5 +73,48 @@ test_that('setVector w/ Virtual Class', {#@testing setVector w/ Virtual Class
     expect_is_exactly(z, vclass@className)
     expect_is(z, velement@className)
 
-    removeVector(vclass)
+    expect_true(removeVector(vclass, where = globalenv()))
+})
+#line 183 "/rdtf/documentation/R/Class-Vector.R"
+test_that('names,refList-method', {#@testing
+    val <- refList('a', 1L, TRUE)
+    expect_is(val, 'refList')
+    expect_is(val, 'envRefClass')
+
+    expect_identical(val[[1]], 'a')
+    expect_identical(val[[2]], 1L)
+    expect_identical(val[[3]], TRUE)
+
+    y <- val
+    y[[4]] <- 'testing'
+    expect_identical(val[[4]], 'testing')
+
+    expect_length(val, 4L)
+    expect_null(names(val))
+})
+#line 388 "/rdtf/documentation/R/Class-Vector.R"
+test_that('setRefVector', {#@testing
+    test_vector <- setRefVector('logical')
+    expect_is(test_vector, 'refObjectGenerator')
+
+    bare <- test_vector()
+    expect_is(bare, "RefVector(logical)")
+    expect_length(bare, 0L)
+    expect_identical(get('element', bare), 'logical')
+
+    val <- test_vector(a=TRUE, b=FALSE)
+    expect_is(val, "RefVector(logical)")
+    expect_identical(get('element', val), 'logical')
+    expect_length(val, 2L)
+    expect_identical(val[[1]], TRUE)
+    expect_identical(val[['a']], TRUE)
+    expect_identical(val[[2]], FALSE)
+    expect_identical(val[['b']], FALSE)
+
+    new <- c(c=NA)
+    val$add(c=NA)
+    expect_length(val, 3L)
+    expect_identical(val[[3]], NA)
+
+    expect_true(val$is_valid())
 })
